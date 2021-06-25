a3228618089b400a6fc8607f61539323fbb8ccc9
natashaval <natasha.valentina1998@gmail.com>
2021-06-25 14:39:16 +0700

    add dynamic feature module

--- app/src/main/java/com/natashaval/pokedex/MainApplication.kt
+++ app/src/main/java/com/natashaval/pokedex/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.natashaval.pokedex
 
 import android.app.Application
+import com.google.android.play.core.splitcompat.SplitCompatApplication
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -8,7 +9,7 @@ import timber.log.Timber
  * Created by natasha.santoso on 19/10/20.
  */
 @HiltAndroidApp
-class MainApplication : Application() {
+class MainApplication : SplitCompatApplication() {
   override fun onCreate() {
     super.onCreate()
     if (BuildConfig.DEBUG) {
